4f4260471f8c5c197ab1789336094e4f805fa8a0
Rodrigo Prado Fontes <fontesr2@gmail.com>
2018-06-17 18:08:59 -0300

    Add model validators

--- api/app/models/currency.rb
+++ api/app/models/currency.rb
@@ -5,4 +5,7 @@ class Currency < ApplicationRecord
 
   has_many :buying_rates, class_name: 'ExchangeRate', foreign_key: :buying_currency_id, dependent: :destroy
   has_many :selling_rates, class_name: 'ExchangeRate', foreign_key: :selling_currency_id, dependent: :destroy
+
+  validates :symbol, presence: true, uniqueness: true
+  validates :currency_type, presence: true
 end
--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
 
   has_many :user_currencies, dependent: :destroy
   has_many :currencies, through: :user_currencies
+
+  validates :first_name, :last_name, presence: true
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  validates :email, presence: true, uniqueness: true, format: VALID_EMAIL_REGEX
 end
