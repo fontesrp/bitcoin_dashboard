72aafb31e7ed722aabb74e4755c4ea5598a32f26
Rodrigo Prado Fontes <fontesr2@gmail.com>
2018-06-17 16:37:05 -0300

    Fix currency associations

--- api/app/models/currency.rb
+++ api/app/models/currency.rb
@@ -3,6 +3,6 @@ class Currency < ApplicationRecord
   has_many :user_currencies, dependent: :destroy
   has_many :users, through: :user_currencies
 
-  has_many :exchange_rates, as: :buying_rates, foreign_key: :buying_currency_id, dependent: :destroy
-  has_many :exchange_rates, as: :selling_rates, foreign_key: :selling_currency_id, dependent: :destroy
+  has_many :buying_rates, class_name: 'ExchangeRate', foreign_key: :buying_currency_id, dependent: :destroy
+  has_many :selling_rates, class_name: 'ExchangeRate', foreign_key: :selling_currency_id, dependent: :destroy
 end
