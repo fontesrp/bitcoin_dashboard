8aeddfce80822341457154a7e0fdacf040c0cbda
Rodrigo Prado Fontes <fontesr2@gmail.com>
2018-06-17 18:10:49 -0300

    Implement token authentication

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::API
+
+  private
+
+  def authenticate_user!
+    head :unauthorized unless current_user.present?
+  end
+
+  def current_user
+
+    token = request.headers['AUTHORIZATION']
+
+    begin
+
+      payload = JWT.decode(
+        token,
+        Rails.application.secrets.secret_key_base
+      )&.first
+
+      if Time.at(payload['exp']) >= Time.now
+        @user ||= User.find_by_id payload['id']
+      else
+        nil
+      end
+
+    rescue JWT::DecodeError => error
+      nil
+    end
+  end
 end
