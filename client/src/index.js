00cfcc62c297d7357855f6162f2d6f2bb31787c3
Rodrigo Prado Fontes <fontesr2@gmail.com>
2018-06-18 17:21:38 -0300

    Setup Redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
 
-import "./index.css";
-import App from "./App";
+import App from "./app/App";
+import rootReducer from "./reducers";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const middleware = applyMiddleware(thunk, logger);
+const store = createStore(rootReducer, middleware);
+
+const app = (
+    <Provider store={ store }>
+        <App />
+    </Provider>
+);
+
+ReactDOM.render(app, document.getElementById("root"));
+
 registerServiceWorker();
